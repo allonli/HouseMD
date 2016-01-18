5b9c31e267ba764898a217c175f1859cf0d359b8
liyonghui <liyonghui@baidu.com>
2016-01-18 22:28:39 +0800

    format code

--- src/main/scala/com/github/zhongl/housemd/command/Trace.scala
+++ src/main/scala/com/github/zhongl/housemd/command/Trace.scala
@@ -16,24 +16,25 @@
 
 package com.github.zhongl.housemd.command
 
-import instrument.Instrumentation
-import management.ManagementFactory
-import com.cedarsoftware.util.io.JsonWriter
-import com.github.zhongl.housemd.misc.ObjUtils
-import com.github.zhongl.yascli.PrintOut
-import com.github.zhongl.housemd.misc.ReflectionUtils._
+import java.io.{BufferedWriter, File, FileWriter}
+import java.lang.instrument.Instrumentation
+import java.lang.management.ManagementFactory
 import java.util.Date
-import collection.immutable.SortedSet
-import com.github.zhongl.housemd.instrument.{Hook, Context}
+
+import com.github.zhongl.housemd.instrument.{Context, Hook}
+import com.github.zhongl.housemd.misc.ObjUtils
+import com.github.zhongl.housemd.misc.ReflectionUtils._
+import com.github.zhongl.yascli.PrintOut
 import org.objectweb.asm.Type
-import java.io.{BufferedWriter, FileWriter, File}
+
+import scala.collection.immutable.SortedSet
 
 /**
- * @author <a href="mailto:zhong.lunfu@gmail.com">zhongl<a>
- */
+  * @author <a href="mailto:zhong.lunfu@gmail.com">zhongl<a>
+  */
 class Trace(val inst: Instrumentation, out: PrintOut)
   extends TransformCommand("trace", "display or output infomation of method invocaton.", inst, out)
-          with MethodFilterCompleter {
+    with MethodFilterCompleter {
 
   val outputRoot = {
     val dir = new File("/tmp/" + name + "/" + ManagementFactory.getRuntimeMXBean.getName)
@@ -41,35 +42,35 @@ class Trace(val inst: Instrumentation, out: PrintOut)
     dir
   }
   val detailFile = new File(outputRoot, "detail")
-  val stackFile  = new File(outputRoot, "stack")
+  val stackFile = new File(outputRoot, "stack")
 
   private val detailable = flag("-d" :: "--detail" :: Nil, "enable append invocation detail to " + detailFile + ".")
   private val detailInJson = flag("-j" :: "--json" :: Nil, "convert detail info into json format.")
-  private val stackable  = flag("-s" :: "--stack" :: Nil, "enable append invocation calling stack to " + stackFile + ".")
+  private val stackable = flag("-s" :: "--stack" :: Nil, "enable append invocation calling stack to " + stackFile + ".")
 
   private val methodFilters = parameter[Array[MethodFilter]]("method-filter", "method filter pattern like \"ClassSimpleName.methodName\" or \"ClassSimpleName\".")
 
-  protected def isCandidate(klass: Class[_]) = methodFilters().find(_.filter(klass)).isDefined
+  protected def isCandidate(klass: Class[_]) = methodFilters().exists(_.filter(klass))
 
   protected def isDecorating(klass: Class[_], methodName: String) =
-    methodFilters().find(_.filter(klass, methodName)).isDefined
+    methodFilters().exists(_.filter(klass, methodName))
 
   override protected def hook = new Hook() {
 
     val enableDetail = detailable()
-    val enableStack  = stackable()
-    val showDetailInJson  = detailInJson()
+    val enableStack = stackable()
+    val showDetailInJson = detailInJson()
 
     implicit val statisticOrdering = Ordering.by((_: Statistic).methodSign)
 
-    var statistics           = SortedSet.empty[Statistic]
-    var maxMethodSignLength  = 0
+    var statistics = SortedSet.empty[Statistic]
+    var maxMethodSignLength = 0
     var maxClassLoaderLength = 0
 
     if (showDetailInJson) ObjUtils.useJsonFormat() else ObjUtils.useToStringFormat()
 
     lazy val detailWriter = new DetailWriter(new BufferedWriter(new FileWriter(detailFile, true)))
-    lazy val stackWriter  = new StackWriter(new BufferedWriter(new FileWriter(stackFile, true)))
+    lazy val stackWriter = new StackWriter(new BufferedWriter(new FileWriter(stackFile, true)))
 
     override def enterWith(context: Context) {}
 
@@ -82,9 +83,9 @@ class Trace(val inst: Instrumentation, out: PrintOut)
       maxClassLoaderLength = math.max(maxClassLoaderLength, statistic.loader.length)
       maxMethodSignLength = math.max(maxMethodSignLength, statistic.methodSign.length)
 
-      statistics = statistics find { _.filter(context) } match {
+      statistics = statistics find {_.filter(context)} match {
         case Some(s) => (statistics - s) + (s + statistic)
-        case None    => statistics + statistic
+        case None => statistics + statistic
       }
     }
 
@@ -122,7 +123,7 @@ class Trace(val inst: Instrumentation, out: PrintOut)
     private lazy val thisObjectString = context match {
       case c if c.thisObject == null => "[Static Method]"
       case c if isInit(c.methodName) => "[Initialize Method]"
-      case _                         => getOrForceToNativeString(context.thisObject)
+      case _ => getOrForceToNativeString(context.thisObject)
     }
 
     private lazy val avgElapseMillis =
@@ -131,11 +132,11 @@ class Trace(val inst: Instrumentation, out: PrintOut)
     def +(s: Statistic) = new Statistic(context, totalTimes + s.totalTimes, totalElapseMills + s.totalElapseMills)
 
     def filter(context: Context) = this.context.loader == context.loader &&
-                                   this.context.className == context.className &&
-                                   this.context.methodName == context.methodName &&
-                                   this.context.arguments.size == context.arguments.size &&
-                                   this.context.descriptor == context.descriptor &&
-                                   (isInit(context.methodName) || this.context.thisObject == context.thisObject)
+      this.context.className == context.className &&
+      this.context.methodName == context.methodName &&
+      this.context.arguments.length == context.arguments.length &&
+      this.context.descriptor == context.descriptor &&
+      (isInit(context.methodName) || this.context.thisObject == context.thisObject)
 
     def reps(maxMethodSignLength: Int, maxClassLoaderLength: Int) =
       "%1$-" + maxMethodSignLength + "s    %2$-" + maxClassLoaderLength + "s    %3$9s    %4$9sms    %5$s" format(
@@ -153,16 +154,16 @@ class Trace(val inst: Instrumentation, out: PrintOut)
 
 class DetailWriter(writer: BufferedWriter) {
   def write(context: Context) {
-    val started = "%1$tF %1$tT" format (new Date(context.started))
+    val started = "%1$tF %1$tT" format new Date(context.started)
     val elapse = "%,dms" format (context.stopped.get - context.started)
     val thread = "[" + context.thread.getName + "]"
     val thisObject = if (context.thisObject == null) "null" else context.thisObject.toString
     val method = context.className + "." + context.methodName
     val arguments = context.arguments.mkString("[" + ObjUtils.parameterSeparator, ObjUtils.parameterSeparator, "]")
     val resultOrExcption = context.resultOrException match {
-      case Some(x)                      => ObjUtils.toString(x)
+      case Some(x) => ObjUtils.toString(x)
       case None if context.isVoidReturn => "void"
-      case None                         => "null"
+      case None => "null"
     }
 
     val argumentsAndResult = "Arguments: " + arguments + ObjUtils.parameterSeparator + "Result: " + resultOrExcption
@@ -178,12 +179,12 @@ class DetailWriter(writer: BufferedWriter) {
           writer.write("\tat " + s)
           writer.newLine()
       }
-      case _                                    =>
+      case _ =>
     }
   }
 
   def close() {
-    try {writer.close()} catch {case _ => }
+    try {writer.close()} catch {case _: Throwable =>}
   }
 }
 
@@ -201,6 +202,6 @@ class StackWriter(writer: BufferedWriter) {
   }
 
   def close() {
-    try {writer.close()} catch {case _ => }
+    try {writer.close()} catch {case _: Throwable =>}
   }
 }
